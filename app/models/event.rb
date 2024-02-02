3bd7027dc78a0ba510e25fc6be7395b9d0451b60
Islam Sulaiman <ma.eslam@yahoo.com>
2024-02-02 19:49:56 +0200

    fix: change spelling of through association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
 
   belongs_to :creator, class_name: "User"
-  has_many :attendees, thought: :attended_events, source: :event_attendee_id
+  has_many :attendees, through: :attended_events, source: :event_attendee_id
 end
