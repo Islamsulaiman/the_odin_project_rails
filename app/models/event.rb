db0287e9a18329d9a1739445109be49c153b9e3d
Islam Sulaiman <ma.eslam@yahoo.com>
2024-02-05 21:55:51 +0200

    fix: fix associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
 
   belongs_to :creator, class_name: "User"
-  has_many :attendees, through: :attendee_events, source: :event_attendee_id
+  has_many :attendees, through: :attendee_events, source: :event_attendee
   has_many :attendee_events, foreign_key: :attended_event_id
 end
