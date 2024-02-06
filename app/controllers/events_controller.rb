13c28cab8a5c78fb6081c2eeea4c4bf563056633
Islam Sulaiman <ma.eslam@yahoo.com>
2024-02-06 06:09:34 +0200

    feat: list the attendees at every show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,7 @@ class EventsController < ApplicationController
 
   # GET /events/1 or /events/1.json
   def show
+    @attended_users = User.find(params[:id]).attendee_events
   end
 
   # GET /events/new
