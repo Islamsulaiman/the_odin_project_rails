354d59a2c2195370c9672d1a9d42be16a649686d
Islam Sulaiman <ma.eslam@yahoo.com>
2024-02-10 14:21:04 +0200

    feat: create the user using the HTML form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(permit_user)
+
+    if @user.save!
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity 
+    end
+  end
+
+  private
+
+  def permit_user
+    params.require(:user).permit(:user_name, :password, :email)
   end
 end
