73fda8d8f08d1fddc1c07c64a0ed8099eaa18ef2
Islam Sulaiman <ma.eslam@yahoo.com>
2024-02-10 15:03:45 +0200

    feat: use form_with helper instead of form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    byebug
     @user = User.create!(permit_user)
 
     if @user.save!
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   private
 
   def permit_user
-    # params.require(:user).permit(:user_name, :password, :email)
-    params.permit(:user_name, :password, :email) #with the form_helper, because it dose'nt nest the objects inside the model name as in form_with helper
+    params.require(:user).permit(:user_name, :password, :email)
+    # params.permit(:user_name, :password, :email) #with the form_helper, because it dose'nt nest the objects inside the model name as in form_with helper
   end
 end
