d1ee7f7a9f1bf9ae4db924ac1173762c8b1945df
Islam Sulaiman <ma.eslam@yahoo.com>
2024-02-10 10:12:40 +0200

    feat: add validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :user_name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum:6 }
 end
